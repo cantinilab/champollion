dc717faf2e33e0ac4ead703b2d5edacf7feddad0
jules-samaran <samaran.jules@gmail.com>
2026-04-17 10:31:46 +0200

    Fix docs (create copy of tutorial nbs at build time to put in source)

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,9 +1,18 @@
 import sys
+import shutil
 from datetime import datetime
 from pathlib import Path
 
 ROOT = Path(__file__).resolve().parents[2]
+DOCS_SOURCE = Path(__file__).resolve().parent
 sys.path.insert(0, str(ROOT / "src"))
+
+ROOT_TUTORIALS = ROOT / "tutorials"
+DOCS_TUTORIALS = DOCS_SOURCE / "tutorials" / "notebooks"
+if ROOT_TUTORIALS.exists():
+    if DOCS_TUTORIALS.is_symlink():
+        DOCS_TUTORIALS.unlink()
+    shutil.copytree(ROOT_TUTORIALS, DOCS_TUTORIALS, dirs_exist_ok=True)
 
 project = "Champollion"
 author = "Jules Samaran"
